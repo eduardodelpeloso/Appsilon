9ea6bdcc525b0f0913c3bce70b408e87a8e53068
Eduardo F. del Peloso <eduardodelpeloso@gmail.com>
2020-10-25 19:51:40 +0100

    Deleted commented out code lines

--- ui.R
+++ ui.R
@@ -6,38 +6,26 @@ ship_types = unique(df$ship_type)
 
 shinyUI(
   semanticPage(
-  
-    #useShinyjs(),
-
-    # Loading message
-    #div(
-    #  id = "loading-content",
-    #  h2("Loading...")
-    #),
-    
-    # The main app code goes here
-    #hidden(
-      div(
-        style="width: 100%; height: 100%; background-color: powderblue",
-        id = "app-content",
-        titlePanel("Shiny Developer @Appsilon - Recruitment Task"),
-        sidebar_layout(
-          sidebar_panel(
-            p("Choose ship type:"),  
-            dropdown_input("simple_dropdown", ship_types, value = ship_types[1],type = "selection fluid"),
-            p(),
-            p("Choose ship:"),
-            uiOutput("second_dropdown"),
-            br(),
-            span(style="color:red; font-size: 20px",
-            p("Distance travelled between displayed points (in meters):"),
-            textOutput("distance_travelled"))
-          ),
-          main_panel(
-            leafletOutput("mymap")
-          )
+    div(
+      style="width: 100%; height: 100%; background-color: powderblue",
+      id = "app-content",
+      titlePanel("Shiny Developer @Appsilon - Recruitment Task"),
+      sidebar_layout(
+        sidebar_panel(
+          p("Choose ship type:"),  
+          dropdown_input("simple_dropdown", ship_types, value = ship_types[1],type = "selection fluid"),
+          p(),
+          p("Choose ship:"),
+          uiOutput("second_dropdown"),
+          br(),
+          span(style="color:red; font-size: 20px",
+               p("Distance travelled between displayed points (in meters):"),
+               textOutput("distance_travelled"))
+        ),
+        main_panel(
+          leafletOutput("mymap")
         )
       )
-    #)
+    )
   )
 )
