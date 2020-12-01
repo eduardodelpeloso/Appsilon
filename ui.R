a9a9b61c7cbec5cf5554c5ca779edfec563b6041
Eduardo Fernandez del Peloso <eduardodelpeloso@gmail.com>
2020-12-01 13:01:54 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,24 +4,26 @@ library(leaflet)
 
 shinyUI(
   semanticPage(
-      div(
-        style="width: 100%; height: 100%; background-color: powderblue",
-        id = "app-content",
-        titlePanel("Shiny Developer @Appsilon - Recruitment Task"),
-        sidebar_layout(
-          sidebar_panel(
-            dropdownUI("ship_type", "Choose ship type:", ship_types, ship_types, ship_types[1]),
-            p(),
-            dropdownUI("ship_name", "Choose ship:", NULL),
-            br(),
-            span(style="color:red; font-size: 20px",
+    div(
+      style = "width: 100%; height: 100%; background-color: powderblue",
+      id = "app-content",
+      titlePanel("Map_Dashboard"),
+      sidebar_layout(
+        sidebar_panel(
+          dropdownUI("ship_type", "Choose ship type:", ship_types, ship_types, ship_types[1]),
+          p(),
+          dropdownUI("ship_name", "Choose ship:", NULL),
+          br(),
+          span(
+            style = "color:red; font-size: 20px",
             p("Distance travelled between displayed points (in meters):"),
-            textOutput("distance_travelled"))
-          ),
-          main_panel(
-            leafletOutput("my_map")
+            textOutput("distance_travelled")
           )
+        ),
+        main_panel(
+          leafletOutput("my_map")
         )
       )
+    )
   )
 )
