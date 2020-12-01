5f5208b45bb705c9f90aeeb2c35ecf028b6c6c69
Eduardo Fernandez del Peloso <eduardodelpeloso@gmail.com>
2020-12-01 13:04:11 +0100

    Update ship_data.R

--- ship_data.R
+++ ship_data.R
@@ -2,7 +2,7 @@
 # observations of a ship. Return lat/lon of the begin and end
 # of this leg, and the distance traveled
 ship_lat_lon <- function(ship_name) {
-  
+
   # Get list of observations of ship_name
   result <- ships[ships$SHIPNAME == ship_name(), ]
 
@@ -11,47 +11,49 @@ ship_lat_lon <- function(ship_name) {
   if (is.null(ship_name()) || ship_name() == "") {
     return(list(NULL, NULL))
   }
-  
-  datetime_begin = strptime(result[1, "DATETIME"], "%Y-%m-%d %H:%M:%S")
-  lat_max_dist_begin = result[1, "LAT"]
-  lon_max_dist_begin = result[1, "LON"]
-  datetime1 = datetime_begin
-  lat1 = lat_max_dist_begin
-  lon1 = lon_max_dist_begin
-  max_dist = 0.0
-  
+
+  datetime_begin <- strptime(result[1, "DATETIME"], "%Y-%m-%d %H:%M:%S")
+  lat_max_dist_begin <- result[1, "LAT"]
+  lon_max_dist_begin <- result[1, "LON"]
+  datetime1 <- datetime_begin
+  lat1 <- lat_max_dist_begin
+  lon1 <- lon_max_dist_begin
+  max_dist <- 0.0
+
   for (row in 2:nrow(result)) {
-    datetime2 = strptime(result[row, "DATETIME"], "%Y-%m-%d %H:%M:%S")
-    lat2 = result[row, "LAT"]
-    lon2 = result[row, "LON"]
-    
-    diftime = as.numeric(difftime(datetime2, datetime1, units = 'secs'))
-    
+    datetime2 <- strptime(result[row, "DATETIME"], "%Y-%m-%d %H:%M:%S")
+    lat2 <- result[row, "LAT"]
+    lon2 <- result[row, "LON"]
+
+    diftime <- as.numeric(difftime(datetime2, datetime1, units = "secs"))
+
     # If time between two observations of the same ship is > 5 minutes
     # then the observation is ignored
     if (abs(diftime) > 300) {
-      lat1 = lat2
-      lon1 = lon2
-      datetime1 = datetime2
-      next      
+      lat1 <- lat2
+      lon1 <- lon2
+      datetime1 <- datetime2
+      next
     }
-    
-    distance = dist_2_pt(lat1, lon1, lat2, lon2)
+
+    distance <- dist_2_pt(lat1, lon1, lat2, lon2)
     if (distance >= max_dist) {
-      max_dist = distance
-      lat_max_dist_begin = lat1
-      lon_max_dist_begin = lon1
-      lat_max_dist_end = lat2
-      lon_max_dist_end = lon2
+      max_dist <- distance
+      lat_max_dist_begin <- lat1
+      lon_max_dist_begin <- lon1
+      lat_max_dist_end <- lat2
+      lon_max_dist_end <- lon2
     }
-    
-    lat1 = lat2
-    lon1 = lon2
-    datetime1 = datetime2
+
+    lat1 <- lat2
+    lon1 <- lon2
+    datetime1 <- datetime2
   }
 
-  return(list(max_dist, array(c(lon_max_dist_begin, lon_max_dist_end, 
-                    lat_max_dist_begin, lat_max_dist_end), c(2, 2))))
+  return(list(max_dist, array(c(
+    lon_max_dist_begin, lon_max_dist_end,
+    lat_max_dist_begin, lat_max_dist_end
+  ), c(2, 2))))
 }
 
 # Convert angle in degrees to radians
@@ -62,17 +64,15 @@ deg2rad <- function(deg) {
 # Calculate the distace in meters between two points in the
 # Earth given their latitude and longitude
 dist_2_pt <- function(lat1, lon1, lat2, lon2) {
-  
-    R = 6371.0
-    
-    lat_distance = deg2rad(lat2 - lat1);
-    lon_distance = deg2rad(lon2 - lon1);
-    a = sin(lat_distance / 2.0) * sin(lat_distance / 2.0) +
-      cos(deg2rad(lat1)) * cos(deg2rad(lat2)) *
-      sin(lon_distance / 2.0) * sin(lon_distance / 2.0)
-    c = 2.0 * atan2(sqrt(a), sqrt(1.0 - a))
-    distance = R * c * 1000.0
-    
-    return(distance)
-}
+  R <- 6371.0
 
+  lat_distance <- deg2rad(lat2 - lat1)
+  lon_distance <- deg2rad(lon2 - lon1)
+  a <- sin(lat_distance / 2.0) * sin(lat_distance / 2.0) +
+    cos(deg2rad(lat1)) * cos(deg2rad(lat2)) *
+      sin(lon_distance / 2.0) * sin(lon_distance / 2.0)
+  c <- 2.0 * atan2(sqrt(a), sqrt(1.0 - a))
+  distance <- R * c * 1000.0
+
+  return(distance)
+}
